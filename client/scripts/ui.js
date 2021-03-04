ddfabaa9653f21eadf38be2e7672b67332eb4ede
yjmp14 <yjmp14@gmail.com>
2021-03-04 11:35:56 +0800

    细节修改

--- client/scripts/ui.js
+++ client/scripts/ui.js
@@ -74,7 +74,7 @@ class PeerUI {
 
     html() {
         return `
-            <label class="column center" title="Click to send files or right click to send a text">
+            <label class="column center" title="点击发送文件、右键发送消息">
                 <input type="file" multiple>
                 <x-icon shadow="1">
                     <svg class="icon"><use xlink:href="#"/></svg>
