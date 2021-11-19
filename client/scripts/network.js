99b4ebca1e5bb46d29e8a37e207d517e9649769a
yjmp14 <yjmp14@gmail.com>
2021-11-19 21:24:18 +0800

    Update network.js

--- client/scripts/network.js
+++ client/scripts/network.js
@@ -518,7 +518,6 @@ class Events {
 RTCPeer.config = {
     'sdpSemantics': 'unified-plan',
     'iceServers': [
-	{urls: 'turn:zp.wulingate.com', username: 'hmzJ0OHZivkod703', credential: 'KDF04PBYD9xHAp0s' },
 	{urls: 'turn:oc.wulingate.com', username: 'hmzJ0OHZivkod703', credential: 'KDF04PBYD9xHAp0s' },
 	{urls: 'turn:km.wulingate.com', username: 'hmzJ0OHZivkod703', credential: 'KDF04PBYD9xHAp0s' }
 	]
